59dd74fe5d6c7d292b19cc131b38d7e7db5da173
xnnba1984 <65501078+xnnba1984@users.noreply.github.com>
2020-09-30 18:55:14 -0400

    Add files via upload

---
+++ distributed computing/doubletdetection_distribute.R
@@ -0,0 +1,173 @@
+library(reticulate)
+library(Matrix)
+library(Seurat)
+library(dplyr)
+library(parallel)
+library(PRROC)
+library(pbapply)
+source('utility.R')
+set.seed(2020)
+
+# read python module
+doubletdetection <- import('doubletdetection')
+np <- import('numpy')
+
+# read one real data
+system.time(sim.data <- readRDS("real_data/pbmc-2ctrl-dm.rds"))
+score.list <- list()
+data <- sim.data[[1]]; dim(data)
+labels <- sim.data[[2]]; table(labels)
+
+##########################################################################
+# distributed computing batch=2
+##########################################################################
+index <- sample(1:dim(data)[2], round(dim(data)[2]/2))
+data1 <- data[,index]; dim(data1)
+data2 <- data[,-index]; dim(data2)
+label1 <- labels[index]; table(label1)
+label2 <- labels[-index]; table(label2)
+
+# doublet detection on two batches
+score2.list <- pblapply(data.chunk, function(x){
+  clf <- doubletdetection$BoostClassifier(n_iters=5L, use_phenograph=FALSE, standard_scaling=TRUE, random_state = 3L)
+  fit <- clf$fit(t(x))
+  p.values <- abs(fit$all_log_p_values_)
+  score <- apply(p.values, 2, mean)
+})
+score2 <- abs(unlist(score2.list, use.names = F))
+
+# calculated auprc and auroc
+labels2 <- ifelse(labels2 == 'doublet', 1, 0); table(labels2)
+fg <- score2[labels2 == 1]
+bg <- score2[labels2 == 0]
+pr <- pr.curve(scores.class0 = fg, scores.class1 = bg, curve = T)
+pr2 <- pr$auc.integral; pr2
+roc <- roc.curve(scores.class0 = fg, scores.class1 = bg, curve = T); roc$auc
+
+# save merged doublet scores
+score2 <- unlist(score2.list)
+labels2 <- c(label1, label2)
+score.list <- append(score.list, list(score2.list))
+
+##########################################################################
+# distributed computing batch = 4
+##########################################################################
+# random shuffle
+index <- sample(1:dim(data)[2], dim(data)[2])
+# split into 4 batches
+chunk <- split(index, cut(seq_along(index), 4, labels = FALSE)) 
+data.chunk <- pblapply(chunk, function(x){
+  return(data[,x])
+})
+labels4 <- labels[unlist(chunk,use.names = F)]
+# doublet detection on 4 batches
+score4.list <- pblapply(data.chunk, function(x){
+  clf <- doubletdetection$BoostClassifier(n_iters=5L, use_phenograph=FALSE, standard_scaling=TRUE, random_state = 3L)
+  fit <- clf$fit(t(x))
+  p.values <- abs(fit$all_log_p_values_)
+  score <- apply(p.values, 2, mean)
+})
+score4<- abs(unlist(score4.list, use.names = F))
+# merge doublet scores
+score4 <- unlist(score4.list, use.names = F)
+
+# calculated auprc and auroc
+labels4 <- ifelse(labels4 == 'doublet', 1, 0); table(labels4)
+fg <- score4[labels4 == 1]
+bg <- score4[labels4 == 0]
+pr <- pr.curve(scores.class0 = fg, scores.class1 = bg, curve = T)
+pr4 <- pr$auc.integral; pr4
+roc <- roc.curve(scores.class0 = fg, scores.class1 = bg, curve = T); roc$auc
+# save scores
+score.list <- append(score.list, list(score4.list))
+
+##########################################################################
+# distributed computing batch = 6
+##########################################################################
+# split into 6 batches
+chunk <- split(index, cut(seq_along(index), 6, labels = FALSE)) 
+data.chunk <- pblapply(chunk, function(x){
+  return(data[,x])
+})
+labels6 <- labels[unlist(chunk,use.names = F)]
+# doublet detection on 6 batches
+score6.list <- pblapply(data.chunk, function(x){
+  clf <- doubletdetection$BoostClassifier(n_iters=5L, use_phenograph=FALSE, standard_scaling=TRUE, random_state = 3L)
+  fit <- clf$fit(t(x))
+  p.values <- abs(fit$all_log_p_values_)
+  score <- apply(p.values, 2, mean)
+})
+score6 <- abs(unlist(score6.list, use.names = F))
+# merge scores of six batches
+score6 <- unlist(score6.list, use.names = F)
+
+# analyze result
+labels6 <- ifelse(labels6 == 'doublet', 1, 0); table(labels6)
+fg <- score6[labels6 == 1]
+bg <- score6[labels6 == 0]
+pr <- pr.curve(scores.class0 = fg, scores.class1 = bg, curve = T)
+pr6 <- pr$auc.integral; pr6
+roc <- roc.curve(scores.class0 = fg, scores.class1 = bg, curve = T); roc$auc
+# save scores
+score.list <- append(score.list, list(score6.list))
+
+##########################################################################
+# distributed computing batch = 8
+##########################################################################
+# random split into 8 batches
+chunk <- split(index, cut(seq_along(index), 8, labels = FALSE)) 
+data.chunk <- pblapply(chunk, function(x){
+  return(data[,x])
+})
+labels8 <- labels[unlist(chunk,use.names = F)]
+# doublet detection on 8 batches
+score8.list <- pblapply(data.chunk, function(x){
+  clf <- doubletdetection$BoostClassifier(n_iters=5L, use_phenograph=FALSE, standard_scaling=TRUE, random_state = 3L)
+  fit <- clf$fit(t(x))
+  p.values <- abs(fit$all_log_p_values_)
+  score <- apply(p.values, 2, mean)
+})
+score8 <- abs(unlist(score8.list, use.names = F))
+# merge scores of 8 batches
+score8 <- unlist(score8.list, use.names = F)
+
+# analyze result
+labels8 <- ifelse(labels8 == 'doublet', 1, 0); table(labels8)
+fg <- score8[labels8 == 1]
+bg <- score8[labels8 == 0]
+pr <- pr.curve(scores.class0 = fg, scores.class1 = bg, curve = T)
+pr8 <- pr$auc.integral; pr8
+roc <- roc.curve(scores.class0 = fg, scores.class1 = bg, curve = T); roc$auc
+# save doublet scores
+score.list <- append(score.list, list(score8.list))
+
+##########################################################################
+# distributed computing batch = 10
+##########################################################################
+# random split into 10 batches
+chunk <- split(index, cut(seq_along(index), 10, labels = FALSE)) 
+data.chunk <- pblapply(chunk, function(x){
+  return(data[,x])
+})
+labels10 <- labels[unlist(chunk,use.names = F)]
+# doublet detection on 10 batches
+score10.list <- pblapply(data.chunk, function(x){
+  clf <- doubletdetection$BoostClassifier(n_iters=5L, use_phenograph=FALSE, standard_scaling=TRUE, random_state = 3L)
+  fit <- clf$fit(t(x))
+  p.values <- abs(fit$all_log_p_values_)
+  score <- apply(p.values, 2, mean)
+})
+# merge doublet scores from 10 batches
+score10 <- unlist(score10.list, use.names = F)
+
+# analyze result
+labels10 <- ifelse(labels10 == 'doublet', 1, 0); table(labels10)
+fg <- score10[labels10 == 1]
+bg <- score10[labels10 == 0]
+pr <- pr.curve(scores.class0 = fg, scores.class1 = bg, curve = T)
+pr10 <- pr$auc.integral; pr10
+roc <- roc.curve(scores.class0 = fg, scores.class1 = bg, curve = T); roc$auc
+# save scores
+score.list <- append(score.list, list(score10.list))
+# save all scores
+system.time(saveRDS(score.list, "sim_result/doubletdetection_distribute.rds"))
---
+++ distributed computing/scds_distribute.R
@@ -0,0 +1,202 @@
+library(Matrix)
+library(Seurat)
+library(scds)
+library(SingleCellExperiment)
+library(PRROC)
+library(pbapply)
+source('utility.R')
+set.seed(2020)
+
+# read one real data
+system.time(sim.data <- readRDS("real_data/pbmc-2ctrl-dm.rds"))
+score.list <- list()
+data <- sim.data[[1]]; dim(data)
+labels <- sim.data[[2]]; table(labels)
+
+#####################################
+# batch=2
+#####################################
+data <- sim.data[[1]]; dim(data)
+labels <- sim.data[[2]]; table(labels)
+index <- sample(1:dim(data)[2], round(dim(data)[2]/2))
+data1 <- data[,index]; dim(data1)
+data2 <- data[,-index]; dim(data2)
+label1 <- labels[index]; table(label1)
+label2 <- labels[-index]; table(label2)
+
+score2.list <- pblapply(list(data1, data2), function(x){
+  sce <- SingleCellExperiment(assays = list(counts = x))
+  sce <- cxds(sce); sce <- bcds(sce); sce <- cxds_bcds_hybrid(sce)
+  CD <- colData(sce)
+  return(list(CD$cxds_score,CD$bcds_score,CD$hybrid_score))
+})
+
+score2.cxds <- c(score2.list[[1]][[1]], score2.list[[2]][[1]])
+score2.bcds <- c(score2.list[[1]][[2]], score2.list[[2]][[2]])
+score2.hybrid <- c(score2.list[[1]][[3]], score2.list[[2]][[3]])
+labels2 <- c(label1, label2)
+
+# analyze result
+labels2 <- ifelse(labels2 == 'doublet', 1, 0); table(labels2)
+fg <- score2.cxds[labels2 == 1]
+bg <- score2.cxds[labels2 == 0]
+pr <- pr.curve(scores.class0 = fg, scores.class1 = bg, curve = T)
+pr2 <- pr$auc.integral; pr2
+roc <- roc.curve(scores.class0 = fg, scores.class1 = bg, curve = T); roc$auc
+
+# save merge prediction result
+score.bcds.list <- append(score.bcds.list, list(score2.bcds))
+score.cxds.list <- append(score.cxds.list, list(score2.cxds))
+score.hybrid.list <- append(score.hybrid.list, list(score2.hybrid))
+
+#####################################
+# batch=4
+#####################################
+chunk <- split(index, cut(seq_along(index), 4, labels = FALSE)) 
+data.chunk <- pblapply(chunk, function(x){
+  return(data[,x])
+})
+labels4 <- labels[unlist(chunk,use.names = F)]
+
+score4.list <- pblapply(data.chunk, function(x){
+  sce <- SingleCellExperiment(assays = list(counts = x))
+  sce <- cxds(sce); sce <- bcds(sce); sce <- cxds_bcds_hybrid(sce)
+  CD <- colData(sce)
+  return(list(CD$cxds_score,CD$bcds_score,CD$hybrid_score))
+})
+score4.cxds <- c(score4.list[[1]][[1]], score4.list[[2]][[1]], score4.list[[3]][[1]], score4.list[[4]][[1]])
+score4.bcds <- c(score4.list[[1]][[2]], score4.list[[2]][[2]], score4.list[[3]][[2]], score4.list[[4]][[2]])
+score4.hybrid <- c(score4.list[[1]][[3]], score4.list[[2]][[3]], score4.list[[3]][[3]], score4.list[[4]][[3]])
+
+# analyze result
+labels4 <- ifelse(labels4 == 'doublet', 1, 0); table(labels4)
+fg <- score4.cxds[labels4 == 1]
+bg <- score4.cxds[labels4 == 0]
+pr <- pr.curve(scores.class0 = fg, scores.class1 = bg, curve = T)
+pr4 <- pr$auc.integral; pr4
+roc <- roc.curve(scores.class0 = fg, scores.class1 = bg, curve = T); roc$auc
+
+# save merge prediction result
+score.bcds.list <- append(score.bcds.list, list(score4.bcds))
+score.cxds.list <- append(score.cxds.list, list(score4.cxds))
+score.hybrid.list <- append(score.hybrid.list, list(score4.hybrid))
+
+#####################################
+# batch=6
+#####################################
+chunk <- split(index, cut(seq_along(index), 6, labels = FALSE)) 
+data.chunk <- pblapply(chunk, function(x){
+  return(data[,x])
+})
+labels6 <- labels[unlist(chunk,use.names = F)]
+
+score6.list <- pblapply(data.chunk, function(x){
+  sce <- SingleCellExperiment(assays = list(counts = x))
+  sce <- cxds(sce); sce <- bcds(sce); sce <- cxds_bcds_hybrid(sce)
+  CD <- colData(sce)
+  return(list(CD$cxds_score,CD$bcds_score,CD$hybrid_score))
+})
+score6.cxds <- c(score6.list[[1]][[1]], score6.list[[2]][[1]], score6.list[[3]][[1]], score6.list[[4]][[1]],
+                 score6.list[[5]][[1]], score6.list[[6]][[1]])
+score6.bcds <- c(score6.list[[1]][[2]], score6.list[[2]][[2]], score6.list[[3]][[2]], score6.list[[4]][[2]],
+                 score6.list[[5]][[2]], score6.list[[6]][[2]])
+score6.hybrid <- c(score6.list[[1]][[3]], score6.list[[2]][[3]], score6.list[[3]][[3]], score6.list[[4]][[3]],
+                   score6.list[[5]][[3]], score6.list[[6]][[3]])
+
+# analyze result
+labels6 <- ifelse(labels6 == 'doublet', 1, 0); table(labels6)
+fg <- score6.cxds[labels6 == 1]
+bg <- score6.cxds[labels6 == 0]
+pr <- pr.curve(scores.class0 = fg, scores.class1 = bg, curve = T)
+pr6 <- pr$auc.integral; pr6
+roc <- roc.curve(scores.class0 = fg, scores.class1 = bg, curve = T); roc$auc
+
+# save merge prediction result
+score.bcds.list <- append(score.bcds.list, list(score6.bcds))
+score.cxds.list <- append(score.cxds.list, list(score6.cxds))
+score.hybrid.list <- append(score.hybrid.list, list(score6.hybrid))
+
+#####################################
+# batch=8
+#####################################
+chunk <- split(index, cut(seq_along(index), 8, labels = FALSE)) 
+data.chunk <- pblapply(chunk, function(x){
+  return(data[,x])
+})
+labels8 <- labels[unlist(chunk,use.names = F)]
+
+score8.list <- pblapply(data.chunk, function(x){
+  sce <- SingleCellExperiment(assays = list(counts = x))
+  sce <- cxds(sce); sce <- bcds(sce); sce <- cxds_bcds_hybrid(sce)
+  CD <- colData(sce)
+  return(list(CD$cxds_score,CD$bcds_score,CD$hybrid_score))
+})
+score8.cxds <- c(score8.list[[1]][[1]], score8.list[[2]][[1]], score8.list[[3]][[1]], score8.list[[4]][[1]],
+                 score8.list[[5]][[1]], score8.list[[6]][[1]],score8.list[[7]][[1]], score8.list[[8]][[1]])
+score8.bcds <- c(score8.list[[1]][[2]], score8.list[[2]][[2]], score8.list[[3]][[2]], score8.list[[4]][[2]],
+                 score8.list[[5]][[2]], score8.list[[6]][[2]],score8.list[[7]][[2]], score8.list[[8]][[2]])
+score8.hybrid <- c(score8.list[[1]][[3]], score8.list[[2]][[3]], score8.list[[3]][[3]], score8.list[[4]][[3]],
+                   score8.list[[5]][[3]], score8.list[[6]][[3]],score8.list[[7]][[3]], score8.list[[8]][[3]])
+
+# analyze result
+labels8 <- ifelse(labels8 == 'doublet', 1, 0); table(labels8)
+fg <- score8.cxds[labels8 == 1]
+bg <- score8.cxds[labels8 == 0]
+pr <- pr.curve(scores.class0 = fg, scores.class1 = bg, curve = T)
+pr8 <- pr$auc.integral; pr8
+roc <- roc.curve(scores.class0 = fg, scores.class1 = bg, curve = T); roc$auc
+
+# save merge prediction result
+score.bcds.list <- append(score.bcds.list, list(score8.bcds))
+score.cxds.list <- append(score.cxds.list, list(score8.cxds))
+score.hybrid.list <- append(score.hybrid.list, list(score8.hybrid))
+
+#####################################
+# batch=10
+#####################################
+chunk <- split(index, cut(seq_along(index), 10, labels = FALSE)) 
+data.chunk <- pblapply(chunk, function(x){
+  return(data[,x])
+})
+labels10 <- labels[unlist(chunk,use.names = F)]
+
+score10.list <- pblapply(data.chunk, function(x){
+  sce <- SingleCellExperiment(assays = list(counts = x))
+  sce <- cxds(sce); sce <- bcds(sce); sce <- cxds_bcds_hybrid(sce)
+  CD <- colData(sce)
+  return(list(CD$cxds_score,CD$bcds_score,CD$hybrid_score))
+})
+score10.cxds <- c(score10.list[[1]][[1]], score10.list[[2]][[1]], score10.list[[3]][[1]], score10.list[[4]][[1]],
+                 score10.list[[5]][[1]], score10.list[[6]][[1]],score10.list[[7]][[1]], score10.list[[8]][[1]],
+                 score10.list[[9]][[1]], score10.list[[10]][[1]])
+score10.bcds <- c(score10.list[[1]][[2]], score10.list[[2]][[2]], score10.list[[3]][[2]], score10.list[[4]][[2]],
+                 score10.list[[5]][[2]], score10.list[[6]][[2]],score10.list[[7]][[2]], score10.list[[8]][[2]],
+                 score10.list[[9]][[2]], score10.list[[10]][[2]])
+score10.hybrid <- c(score10.list[[1]][[3]], score10.list[[2]][[3]], score10.list[[3]][[3]], score10.list[[4]][[3]],
+                   score10.list[[5]][[3]], score10.list[[6]][[3]],score10.list[[7]][[3]], score10.list[[8]][[3]],
+                   score10.list[[9]][[3]], score10.list[[10]][[3]])
+
+# analyze result
+labels10 <- ifelse(labels10 == 'doublet', 1, 0); table(labels10)
+fg <- score10.hybrid[labels10 == 1]
+bg <- score10.hybrid[labels10 == 0]
+pr <- pr.curve(scores.class0 = fg, scores.class1 = bg, curve = T)
+pr10 <- pr$auc.integral; pr10
+roc <- roc.curve(scores.class0 = fg, scores.class1 = bg, curve = T); roc$auc
+
+# save merge prediction result
+score.bcds.list <- append(score.bcds.list, list(score10.bcds))
+score.cxds.list <- append(score.cxds.list, list(score10.cxds))
+score.hybrid.list <- append(score.hybrid.list, list(score10.hybrid))
+score.list <- list(score.cxds.list, score.bcds.list, score.hybrid.list)
+# save all scores
+system.time(saveRDS(score.list, "sim_result/scds_distribute_pbmc-2ctrl-dm.rds"))
+
+
+
+
+
+
+
+
+
